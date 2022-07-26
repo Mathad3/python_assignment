8863beb0adc0293a685ab1e276f531806ed8ec2c
apoorva <apoorva.math@gamil.com>
2022-07-26 01:33:59 -0700

    .

---
+++ Untitled-2.py
@@ -0,0 +1,25 @@
+a=10
+print("Type of a:",type(a))
+
+# list1=[1,2,3]
+# list2=[4,5,6]
+# list3=list1+list2
+# print(list3)
+# list3.append(7)
+# print(list3)
+
+# list3.clear()
+# print(list3)
+# set1=("I am apoorva")
+
+
+# print("\n element of set :")
+# for i in set1:
+#     print(i, end=' ')
+Dict= {}
+print("Emoty Dictionary")
+print(Dict)
+Dict2= {125:"Comtech",'Location':"Dharwad"}
+Dict2=['location'] = 
+string1= Dict2[125]+Dict2['Location']
+print(string1)
---
+++ to th to the python world'.py
@@ -0,0 +1,1 @@
+to th to the python world'
