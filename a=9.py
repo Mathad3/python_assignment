a374e08ae10e533e8380c56d1e93130c8b2c5d2c
Mathad3 <109080225+Mathad3@users.noreply.github.com>
2022-07-28 06:24:48 -0700

    .

--- a=9.py
+++ a=9.py
@@ -7,11 +7,23 @@
 # print(a>=b,"greater or equal too")
 # print(a<=b,"less or equal too")
 
-a=10
-b=4
-print(a&b)
-print(a|b)
-print(~a)
-print(a^b)
-print(a>>2)
-print(a<<2)
+
+
+
+# a=2
+# b=3
+# c=a
+# print(a is not b)
+# print(id(a))
+# print(id(b))
+x=24
+y=20
+list=[10,20,30,40,50]
+if(x not in list):
+    print("x is NOT present in given list")
+else:
+    print("x is Present in the list")
+if(y in list):
+    print("y is present in the given list")
+else:
+    print("y is not present in the given list")
