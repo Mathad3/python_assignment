63385ca35a3870c33c222654848e891e4fa654a7
Mathad3 <109080225+Mathad3@users.noreply.github.com>
2022-07-28 05:54:09 -0700

    .

---
+++ a=9.py
@@ -0,0 +1,17 @@
+# a=14
+# b=34
+# print(a>b,"greater")
+# print(a<b,"less than")
+# print(a==b,"equal too") 
+# print(a!=b,"not equal too")
+# print(a>=b,"greater or equal too")
+# print(a<=b,"less or equal too")
+
+a=10
+b=4
+print(a&b)
+print(a|b)
+print(~a)
+print(a^b)
+print(a>>2)
+print(a<<2)
